da3e81d515126b204c39a50421f6a59907b14728
Joseph Murphy <murphydjoseph@gmail.com>
2018-11-24 10:25:33 -0500

    Revert "initial commit"
    This reverts commit 66d5ae0c6f760c57b08de873f8b5488a488f57f3.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,25 +1,3 @@
-// https://github.com/rails/webpacker/blob/master/docs/webpack.md
-// https://github.com/rails/webpacker/issues/371
-// https://hype.codes/how-assemble-rails-frontend-using-webpacker
-
 const { environment } = require('@rails/webpacker')
-
-const jsonLoader = require('./loaders/json')
-
-// Insert json loader at the top of list
-environment.loaders.prepend('json', jsonLoader)
-
-// Insert json loader after/before a given loader
-environment.loaders.insert('json', jsonLoader, { after: 'style'} )
-environment.loaders.insert('json', jsonLoader, { before: 'babel'} )
-
-const url = require('./loaders/url')
-const typescript = require('./loaders/typescript')
-
-environment.loaders.prepend('url', url)
-environment.loaders.prepend('typescript', typescript)
-
-// avoid using both file and url loaders
-environment.loaders.get('file').test = /\.(tiff|ico|svg|eot|otf|ttf|woff|woff2)$/i
 
 module.exports = environment
